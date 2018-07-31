306e2f8e150d4190611838e35c07691c347d4fd9
Mark Knol <mark.knol@gmail.com>
2018-07-31 14:09:18 +0200

    Update console-log-viewer.js
    more important font

--- console-log-viewer.js
+++ console-log-viewer.js
@@ -275,7 +275,8 @@ var ConsoleLogViewer = (function() {
 	
 	ConsoleLogViewer.prototype.addCSS = function()
 	{
-		var css = '#debug_console { background: rgba(0,0,0,.75); font: 11px Arial, sans-serif!important; position:fixed; padding:0; margin:0; z-index:12834567; box-sizing:border-box; pointer-events:none; text-align:left; text-transform:none; }';
+		var css = '#debug_console { background: rgba(0,0,0,.75); position:fixed; padding:0; margin:0; z-index:12834567; box-sizing:border-box; pointer-events:none; text-align:left; text-transform:none; }';
+		css += '#debug_console, #debug_console * { font: 11px Arial, sans-serif!important;  }';
 		css += '#debug_console_messages { background:transparent;pointer-events:none; }'
 		css += '#debug_console_button { border:1px solid #fff; position:absolute; z-index:2;  }';
 		css += '#debug_console.top-aligned {left:0; right:0; top:0;}';
